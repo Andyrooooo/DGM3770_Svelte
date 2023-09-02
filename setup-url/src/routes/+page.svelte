0a3a18bd477ab2213d46fb3aec0f0f1f57f7e75d
Anyrooooo <10919987@my.uvu.edu>
2023-09-01 22:14:07 -0600

    finished: nested components

--- setup-url/src/routes/+page.svelte
+++ setup-url/src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	let name = 'Svelte';
 	let src = 'https://i.pinimg.com/originals/7f/e1/a3/7fe1a3c59fad552cd5346451cbf51e4b.jpg';
 	let doggo = 'doggo';
+
+	import Practice from './practicePage.svelte';
 </script>
 
 <h1>Hello {name}</h1>
@@ -9,6 +11,8 @@
 <img {src} alt="A cute {doggo}" />
 
 <p>This is a paragraph</p>
+
+<Practice />
 
 <style>
 	p {
