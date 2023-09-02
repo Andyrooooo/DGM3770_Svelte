558042e5efeb89c960a13e8e4f33295ccdf48311
Anyrooooo <10919987@my.uvu.edu>
2023-09-01 22:28:47 -0600

    Finished: Assignments

--- setup-url/src/routes/+page.svelte
+++ setup-url/src/routes/+page.svelte
@@ -2,25 +2,29 @@
 	let name = 'Svelte';
 	let src = 'https://i.pinimg.com/originals/7f/e1/a3/7fe1a3c59fad552cd5346451cbf51e4b.jpg';
 	let doggo = 'doggo';
-
 	import Practice from './practicePage.svelte';
-
 	let string = `Svelte is so <strong>COOOOOLLLL!!!<strong/>`;
+
+	let count = 0;
+	function increment() {
+		count += 1;
+	}
 </script>
 
 <h1>Hello {name}</h1>
-
 <img {src} alt="A cute {doggo}" />
-
 <p class="firstParagraph">This is a paragraph</p>
-
 <Practice />
-
 <p>{@html string}</p>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
 
 <style>
 	p.firstParagraph {
-		color: goldenrod;
+		color: red;
 		font-family: 'Comic Sans MS', cursive;
 		font-size: 2em;
 	}
