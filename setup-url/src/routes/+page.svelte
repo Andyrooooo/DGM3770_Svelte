2ea5e4484819400ef9fa25c0974dbdc4b876be41
Anyrooooo <10919987@my.uvu.edu>
2023-09-02 14:39:09 -0600

    finished: each blocks

--- setup-url/src/routes/+page.svelte
+++ setup-url/src/routes/+page.svelte
@@ -67,11 +67,14 @@
 	// 	count += 1;
 	// }
 
-	let count = 0;
+	/* let count = 0;
 
 	function increment() {
 		count += 1;
-	}
+	} */
+
+	const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+	let selected = colors[0];
 </script>
 
 <!-- Introduction html ------------------------------------------------------------------------------>
@@ -123,10 +126,10 @@
 	<p>{count} is greater than 10</p>
 {/if} -->
 
-<button on:click={increment}>
+<!-- <button on:click={increment}>
 	Clicked {count}
 	{count === 1 ? 'time' : 'times'}
-</button>
+</button> -->
 
 <!-- {#if count > 10}
 	<p>{count} is greater than 10</p>
@@ -134,13 +137,23 @@
 	<p>{count} is between 0 and 10</p>
 {/if} -->
 
-{#if count > 10}
+<!-- {#if count > 10}
 	<p>{count} is greater than 10</p>
 {:else if count < 5}
 	<p>{count} is less than 5</p>
 {:else}
 	<p>{count} is between 0 and 10</p>
-{/if}
+{/if} -->
+
+<h1 style="color: {selected}">Pick a color</h1>
+
+<div>
+	{#each colors as color, i}
+		<button aria-current={selected === color} aria-label={color} on:click={() => (selected = color)}
+			>{i + 1}</button
+		>
+	{/each}
+</div>
 
 <style>
 	/* introduction CSS ---------------------------------------------------- */
@@ -149,4 +162,31 @@
 		font-family: 'Comic Sans MS', cursive;
 		font-size: 2em;
 	} */
+
+	/* LOGIC CSS --------------------------------------------------------------------------------- */
+	h1 {
+		transition: color 0.2s;
+	}
+
+	div {
+		display: grid;
+		grid-template-columns: 10%;
+		grid-gap: 5px;
+		max-width: 400px;
+	}
+
+	button {
+		aspect-ratio: 1;
+		border-radius: 50%;
+		background: var(--color, #fff);
+		transform: translate(-2px -2px);
+		filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.2));
+		transition: all 0.1s;
+	}
+
+	button[aria-current='true'] {
+		transform: none;
+		filter: none;
+		box-shadow: inset 3px 3px 4px rgba(0, 0, 0, 0.2);
+	}
 </style>
