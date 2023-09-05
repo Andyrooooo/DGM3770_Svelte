6a44bc212189bb0ffe7f9cbde88577210c9552ee
Anyrooooo <10919987@my.uvu.edu>
2023-09-05 11:21:42 -0600

    adding commits code that was not yet committed, and changing file name

--- svelte_practice/src/routes/utils.js
+++ svelte_practice/src/routes/utils.js
@@ -3,6 +3,7 @@
 export async function getRandomNumber() {
 	// Fetch a random number between 0 and 100
 	// (with a delay, so that we can see it)
+	// API to display random number
 	const res = await fetch(
 		'https://www.random.org/integers/?num=1&min=1&max=100&col=5&base=10&format=plain&rnd=new'
 	);
