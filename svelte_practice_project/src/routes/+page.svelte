0e16bcaa3078154f3883a06a15f9ac3e8b893914
Anyrooooo <10919987@my.uvu.edu>
2023-09-13 10:40:34 -0600

    added links to individual sections and their respective modules

--- svelte_practice_project/src/routes/+page.svelte
+++ svelte_practice_project/src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <h1>Hello this is my svelte tutorial</h1>
 <p>The introduction tutorial will be located <a href="/introduction" class="link">here.</a></p>
-<p>The reactivity tutorial will be located at /reactivity</p>
-<p>The props tutorial will be located at /props</p>
-<p>The logic tutorial will be located at /logic</p>
+<p>The reactivity tutorial will be located <a href="/reactivity" class="link">here.</a></p>
+<p>The props tutorial will be located <a href="/props" class="link">here.</a></p>
+<p>The logic tutorial will be located <a href="/logic" class="link">here.</a></p>
 
 <style>
 	.link {
