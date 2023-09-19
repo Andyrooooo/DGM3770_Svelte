9c4f36e5bb39adf69ae29fadb62967752a908772
Anyrooooo <10919987@my.uvu.edu>
2023-09-19 15:28:47 -0600

    finished: DOM events

--- svelte_practice_project/src/routes/+page.svelte
+++ svelte_practice_project/src/routes/+page.svelte
@@ -3,6 +3,9 @@
 <p>The reactivity tutorial will be located <a href="/reactivity" class="link">here.</a></p>
 <p>The props tutorial will be located <a href="/props" class="link">here.</a></p>
 <p>The logic tutorial will be located <a href="/logic" class="link">here.</a></p>
+<p>The Events tutorial will be located <a href="/events" class="link">here.</a></p>
+
+<!-- dont forget when you add <script lang="ts"><script> -->
 
 <style>
 	.link {
---
+++ svelte_practice_project/src/routes/events/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+	let m = { x: 0, y: 0 }
+
+    let n = { x: 0, y: 0 }
+
+	function handleMove(event) {
+		m.x = Math.trunc(event.clientX)
+		m.y = Math.trunc(event.clientY)
+	}
+
+    function handleMoveTwo(event) {
+		n.x = Math.trunc(event.clientX)
+		n.y = Math.trunc(event.clientY)
+	}
+
+    function disappear(event) {
+        event.target.style.display = "none";
+    }
+
+    function changeColor(event) {
+        event.target.style.background = "green";
+    }
+</script>
+
+<div class="boxOne" on:pointermove={handleMove}>
+	<p>The pointer is at {m.x} x {m.y}</p>
+</div>
+
+<div class="boxTwo" on:pointermove={handleMoveTwo}>
+	<p>The pointer is at {n.x} x {n.y}</p>
+</div>
+
+<button class="boxThree" on:click={disappear}>
+	<p>Don't Click me</p>
+</button>
+
+<button class="boxFour" on:click={changeColor}>
+	<p>Don't click me either</p>
+</button>
+
+<style>
+	div.boxOne {
+		position: fixed;
+		left: .5%;
+		top: 1%;
+		width: 23.5%;
+		height: 23.5%;
+		padding: 1rem;
+        background: rgb(71, 71, 71);
+        border-radius: 10px;
+	}
+    div p {
+        text-align: center;
+    }
+    div.boxTwo{
+		position: fixed;
+		left: 25.5%;
+		top: 1%;
+		width: 23.5%;
+		height: 23.5%;
+		padding: 1rem;
+        background: rgb(71, 71, 71);
+        border-radius: 10px;
+	}
+    button.boxThree{
+		position: fixed;
+		left: 50.5%;
+		top: 1%;
+		width: 23.5%;
+		height: 23.5%;
+		padding: 1rem;
+        background: rgb(71, 71, 71);
+        border-radius: 10px;
+	}
+    button.boxFour{
+		position: fixed;
+		left: 75.5%;
+		top: 1%;
+		width: 23.5%;
+		height: 23.5%;
+		padding: 1rem;
+        background: rgb(71, 71, 71);
+        border-radius: 10px;
+	}
+</style>
