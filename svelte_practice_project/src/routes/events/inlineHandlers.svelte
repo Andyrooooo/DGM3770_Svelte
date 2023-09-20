a3ce2371a1479b93b4f8a7572e93c741045a7ae7
Anyrooooo <10919987@my.uvu.edu>
2023-09-19 23:25:22 -0600

    added additional functionality to inline handlers

--- svelte_practice_project/src/routes/events/inlineHandlers.svelte
+++ svelte_practice_project/src/routes/events/inlineHandlers.svelte
@@ -1,5 +1,8 @@
 <script>
     	let m = { x: 0, y: 0 }
+
+        let message = ''
+
 </script>
 
 <div class="inline">
@@ -7,11 +10,12 @@
     <div class="pointer" on:pointermove={(e) => {m = { x: Math.trunc(e.clientX), y: Math.trunc(e.clientY)}}}>
         The pointer is at {m.x} x {m.y} inside the box
     </div>
+    <div class="hover" on:mouseover={(e) => {message = 'the cursor is in the box'}} on:mouseout={(e) => {message = 'the cursor is outside the box'}}>{message}</div>
 </div>
 
 <style>
     .pointer {
-		width: 100%;
+		flex-basis: 49%;
 		padding: 2rem;
         background: grey;
         text-align: center;
@@ -20,10 +24,19 @@
     .inline {
         display: flex;
         flex-wrap: wrap;
+        justify-content: space-between;
         background-color: rgb(211, 211, 211);
         padding: 1rem;
     }
     .inline h2 {
         color: black;
+        flex-basis: 100%;
+    }
+    .hover {
+        flex-basis: 49%;
+        padding: 2rem;
+        background: grey;
+        text-align: center;
+        border-radius: 10px;
     }
 </style>
