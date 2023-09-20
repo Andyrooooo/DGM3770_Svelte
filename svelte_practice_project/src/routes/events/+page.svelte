c963c7ab6aab912de3e0090df0d27160b9a7b97c
Anyrooooo <10919987@my.uvu.edu>
2023-09-20 11:36:08 -0600

    finished: Event Modifiers

--- svelte_practice_project/src/routes/events/+page.svelte
+++ svelte_practice_project/src/routes/events/+page.svelte
@@ -2,10 +2,15 @@
 	import Dom from './domEvents.svelte'
 
     import Inline from './inlineHandlers.svelte'
+
+    import Modifiers from './eventModifiers.svelte'
 </script>
 
 <Dom />
+
 <Inline />
+
+<Modifiers />
 
 
 <style>
