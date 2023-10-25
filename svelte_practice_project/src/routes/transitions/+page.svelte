027f5a1d70c1e33aa486a2470b98a21effe40e56
Anyrooooo <10919987@my.uvu.edu>
2023-10-25 11:53:34 -0600

    finished: Custom JS Transitions

--- svelte_practice_project/src/routes/transitions/+page.svelte
+++ svelte_practice_project/src/routes/transitions/+page.svelte
@@ -1,11 +1,14 @@
 <script>
     import InOut from './inAndOut.svelte'
 
-    import Transitions from './customTransitions.svelte'
+    import CSSTransitions from './customCSSTransitions.svelte'
+
+    import JSTransitions from './customJSTransitions.svelte'
 </script>
 
 
 <div class="mx-4 mt-4 mb-8 p-4 shadow-md rounded-md bg-white">
     <InOut />
-    <Transitions />
+    <CSSTransitions />
+    <JSTransitions />
 </div>
---
+++ svelte_practice_project/src/routes/transitions/customJSTransitions.svelte
@@ -0,0 +1,44 @@
+<script>
+    let visible = true
+
+    function typewriter(node, { speed = 1 }) {
+		const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE
+
+		if (!valid) {
+			throw new Error(`This transition only works on elements with a single text node child`)
+		}
+
+		const text = node.textContent
+        const duration = text.length / (speed * 0.01)
+
+        return {
+            duration,
+            tick: (t) => {
+                const i = Math.trunc(text.length *t)
+                node.textContent = text.slice(0, i)
+            }
+        }
+	}
+</script>
+
+
+<div class="hover:shadow-lg bg-slate-100 text-black p-2 rounded-lg mt-4">
+
+    <h1 class="transitionsTitle">Custom JS transitions</h1>
+    
+    <label>
+        <input type="checkbox" bind:checked={visible} />
+        visible
+    </label>
+    
+    {#if visible}
+        <p transition:typewriter>
+            The quick brown fox jumps over the lazy dog
+        </p>
+    {/if}
+</div>
+
+
+<style>
+    .transitionsTitle {border-bottom: 1px solid rgb(193, 193, 193);}
+</style>
